66b1862749d19baf084283a20bb26b1744c5715e
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-12 21:43:28 -0500

    feat: mas pruebas

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import styledComponents from 'styled-components';
 import Header from '../components/Header';
 import Slider from '../components/Slider';
-
-const Fondo = styledComponents.div`
-  background: #f5f5f5;
-`;
 
 const HomePage = () => (
   <div>
     <Header />
-    <Fondo>
-      <Slider />
-    </Fondo>
+    <Slider />
+    <Slider />
   </div>
 );
 
